dd0225fbe7cf63fb633680b9e04b5372bc836477
Aditya Rathore <126133554+Aditya24Rathore@users.noreply.github.com>
2025-05-12 19:16:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const music = new Audio();
 
 const songs = [
     {
-        path: 'assets/1.mp3',
+        path: 'https://github.com/Aditya24Rathore/mp/blob/main/assists/1.mp3',
         displayName: 'The Charmer\'s Call',
-        cover: 'assets/1.jpg',
+        cover: 'https://github.com/Aditya24Rathore/mp/blob/main/assists/1.jpg',
         artist: 'Hanu Dixit',
     },
     {
